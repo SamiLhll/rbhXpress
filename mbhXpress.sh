2c55c29e6e9ecb6b8850243dcb4386cb52250ad3
Sami El Hilali <37333782+SamiLhll@users.noreply.github.com>
2022-10-28 17:13:06 +0200

    Update mbhXpress.sh

--- mbhXpress.sh
+++ mbhXpress.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #######################################################
-# mbhXpress v1.0.0
+# mbhXpress v1.2.0
 # Sami El Hilali
-# 2022_octobre_22
+# 2022_octobre_28
 #######################################################
 
 # This scrips takes two protein sets (fasta) and blast (diamond blastp)
@@ -19,13 +19,13 @@ do
 	b) PROTEOME2=${OPTARG};;
 	o) OUTPUT_NAME=${OPTARG};;
 	t) THREADS=${OPTARG};;
-	h) echo "mbhXpress v1.0.0 - usage :";
+	h) echo "mbhXpress v1.2.0 - usage :";
 	   echo "-a PROTEOME1";
 	   echo "-b PROTEOME2";
 	   echo "-o OUTPUT_NAME";
 	   echo "-t THREADS";
 	   exit;;
-	?) echo "mbhXpress v1.0.0 - usage :";
+	?) echo "mbhXpress v1.2.0 - usage :";
 	   echo "-a PROTEOME1";
 	   echo "-b PROTEOM2";
 	   echo "-o OUTPUT_NAME";
@@ -49,7 +49,7 @@ fi
 run_id=($(echo "$RANDOM"))
 > LOG.$OUTPUT_NAME
 echo "---------------------------------------------" | tee -a LOG.$OUTPUT_NAME
-echo "             mbhXpress v1.0.0" | tee -a LOG.$OUTPUT_NAME
+echo "             mbhXpress v1.2.0" | tee -a LOG.$OUTPUT_NAME
 echo "---------------------------------------------" | tee -a LOG.$OUTPUT_NAME
 
 ######################### RUN
@@ -63,23 +63,17 @@ echo " - creating databases : DONE" | tee -a LOG.$OUTPUT_NAME
 
 # Run the blast :
 echo " - run the blast : a vs b" | tee -a LOG.$OUTPUT_NAME
-$diamond_path blastp -q $PROTEOME2 -d "$run_id.p1" -o "$run_id.p2_p1" -k 1 --threads $THREADS &> LOG.$OUTPUT_NAME
+$diamond_path blastp -q $PROTEOME2 -d "$run_id.p1" -o "$run_id.p2_p1" -k 1 -e 1E-10 --threads $THREADS &> LOG.$OUTPUT_NAME
 echo " - run the blast : b vs a" | tee -a LOG.$OUTPUT_NAME
-$diamond_path blastp -q $PROTEOME1 -d "$run_id.p2" -o "$run_id.p1_p2" -k 1 --threads $THREADS &> LOG.$OUTPUT_NAME
+$diamond_path blastp -q $PROTEOME1 -d "$run_id.p2" -o "$run_id.p1_p2" -k 1 -e 1E-10 --threads $THREADS &> LOG.$OUTPUT_NAME
 
 echo " - run the blast : DONE" | tee -a LOG.$OUTPUT_NAME
 
-# select only the significant hits, then select the highest bitscore for the query :
-echo " - compute significant hits" | tee -a LOG.$OUTPUT_NAME
-
-awk '($11 <= 1e-10) { print $1"\t"$2"\t"$12}' $run_id.p2_p1 | \
-awk '$3>max[$1]{max[$1]=$3; row[$1]=$1"\t"$2} END {for (i in row) print row[i]}' > $run_id.p2_p1.s
-
-awk '($11 <= 1e-10) { print $1"\t"$2"\t"$12}' $run_id.p1_p2 | \
-awk '$3>max[$1]{max[$1]=$3; row[$1]=$1"\t"$2} END {for (i in row) print row[i]}' > $run_id.p1_p2.s
-
-# select the Mutual Best hits :
+# select the mutual best hits :
 echo " - select the mutual best hits" | tee -a LOG.$OUTPUT_NAME
+
+cut -f1,2,12 $run_id.p2_p1 | awk '$3>max[$1]{max[$1]=$3; row[$1]=$1"\t"$2} END {for (i in row) print row[i]}' > $run_id.p2_p1.s
+cut -f1,2,12 $run_id.p1_p2 | awk '$3>max[$1]{max[$1]=$3; row[$1]=$1"\t"$2} END {for (i in row) print row[i]}' > $run_id.p1_p2.s
 
 comm -12 <(awk '{print $2"\t"$1}' $run_id.p2_p1.s | sort -k1) <(awk '{print $1"\t"$2}' $run_id.p1_p2.s | sort -k1) \
 > $OUTPUT_NAME
@@ -93,5 +87,6 @@ amount_mbh=($(wc -l $OUTPUT_NAME))
 echo "---------------------------------------------" | tee -a LOG.$OUTPUT_NAME
 echo "   Done : found $amount_mbh mutual best hits." | tee -a LOG.$OUTPUT_NAME
 echo "   Output written in $OUTPUT_NAME" | tee -a LOG.$OUTPUT_NAME
+echo "   Log written in LOG.$OUTPUT_NAME" | tee -a LOG.$OUTPUT_NAME
 echo "---------------------------------------------" | tee -a LOG.$OUTPUT_NAME
 ######################### DONE
