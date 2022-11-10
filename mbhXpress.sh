e9f4afb7a6f9f698d0e59d18246885e212fbed6a
Sami El Hilali <37333782+SamiLhll@users.noreply.github.com>
2022-11-10 16:49:11 +0100

    version 1.2.1

--- mbhXpress.sh
+++ mbhXpress.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 #######################################################
-# mbhXpress v1.2.0
+# mbhXpress v1.2.1
 # Sami El Hilali
-# 2022_octobre_28
+# 2022_november_10
 #######################################################
 
-# This scrips takes two protein sets (fasta) and blast (diamond blastp)
-# each one against the other to get the mutual best hits
+# This scrips takes two protein sets (fasta) and performs a blast (diamond blastp)
+# of each one against the other to get the mutual best hits
 
 ####### PARSE parameters
 
@@ -19,13 +19,13 @@ do
 	b) PROTEOME2=${OPTARG};;
 	o) OUTPUT_NAME=${OPTARG};;
 	t) THREADS=${OPTARG};;
-	h) echo "mbhXpress v1.2.0 - usage :";
+	h) echo "mbhXpress v1.2.1 - usage :";
 	   echo "-a PROTEOME1";
 	   echo "-b PROTEOME2";
 	   echo "-o OUTPUT_NAME";
 	   echo "-t THREADS";
 	   exit;;
-	?) echo "mbhXpress v1.2.0 - usage :";
+	?) echo "mbhXpress v1.2.1 - usage :";
 	   echo "-a PROTEOME1";
 	   echo "-b PROTEOM2";
 	   echo "-o OUTPUT_NAME";
@@ -35,8 +35,19 @@ do
 done
 
 #########################
-# exit if a command fails
+#### exit if :
+# A command fails :
 set -e
+# The PROTEOME1 doesn't exist :
+if [ ! -f "$PROTEOME1" ]; then
+    echo "$PROTEOME1 does not exist.";
+    exit;
+fi
+# The PROTEOME2 doesn't exist :
+if [ ! -f "$PROTEOME2" ]; then
+    echo "$PROTEOME2 does not exist."
+    exit;
+fi
 ########
 
 # More options :
@@ -47,30 +58,30 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 fi
 
 run_id=($(echo "$RANDOM"))
-> LOG.$OUTPUT_NAME
-echo "---------------------------------------------" | tee -a LOG.$OUTPUT_NAME
-echo "             mbhXpress v1.2.0" | tee -a LOG.$OUTPUT_NAME
-echo "---------------------------------------------" | tee -a LOG.$OUTPUT_NAME
+> $OUTPUT_NAME.log
+echo "---------------------------------------------" | tee -a $OUTPUT_NAME.log
+echo "             mbhXpress v1.2.1" | tee -a $OUTPUT_NAME.log
+echo "---------------------------------------------" | tee -a $OUTPUT_NAME.log
 
 ######################### RUN
 
 # create the blast databases :
-echo " - creating databases" | tee -a LOG.$OUTPUT_NAME
-$diamond_path makedb --in $PROTEOME1 -d "$run_id.p1" &> LOG.$OUTPUT_NAME
-$diamond_path makedb --in $PROTEOME2 -d "$run_id.p2" &> LOG.$OUTPUT_NAME
+echo " - creating databases" | tee -a $OUTPUT_NAME.log
+$diamond_path makedb --in $PROTEOME1 -d "$run_id.p1" &> $OUTPUT_NAME.log
+$diamond_path makedb --in $PROTEOME2 -d "$run_id.p2" &> $OUTPUT_NAME.log
 
-echo " - creating databases : DONE" | tee -a LOG.$OUTPUT_NAME
+echo " - creating databases : DONE" | tee -a $OUTPUT_NAME.log
 
 # Run the blast :
-echo " - run the blast : a vs b" | tee -a LOG.$OUTPUT_NAME
-$diamond_path blastp -q $PROTEOME2 -d "$run_id.p1" -o "$run_id.p2_p1" -k 1 -e 1E-10 --threads $THREADS &> LOG.$OUTPUT_NAME
-echo " - run the blast : b vs a" | tee -a LOG.$OUTPUT_NAME
-$diamond_path blastp -q $PROTEOME1 -d "$run_id.p2" -o "$run_id.p1_p2" -k 1 -e 1E-10 --threads $THREADS &> LOG.$OUTPUT_NAME
+echo " - run the blast : a vs b" | tee -a $OUTPUT_NAME.log
+$diamond_path blastp -q $PROTEOME2 -d "$run_id.p1" -o "$run_id.p2_p1" -k 1 -e 1E-10 --threads $THREADS &> $OUTPUT_NAME.log
+echo " - run the blast : b vs a" | tee -a $OUTPUT_NAME.log
+$diamond_path blastp -q $PROTEOME1 -d "$run_id.p2" -o "$run_id.p1_p2" -k 1 -e 1E-10 --threads $THREADS &> $OUTPUT_NAME.log
 
-echo " - run the blast : DONE" | tee -a LOG.$OUTPUT_NAME
+echo " - run the blast : DONE" | tee -a $OUTPUT_NAME.log
 
 # select the mutual best hits :
-echo " - select the mutual best hits" | tee -a LOG.$OUTPUT_NAME
+echo " - select the mutual best hits" | tee -a $OUTPUT_NAME.log
 
 cut -f1,2,12 $run_id.p2_p1 | awk '$3>max[$1]{max[$1]=$3; row[$1]=$1"\t"$2} END {for (i in row) print row[i]}' > $run_id.p2_p1.s
 cut -f1,2,12 $run_id.p1_p2 | awk '$3>max[$1]{max[$1]=$3; row[$1]=$1"\t"$2} END {for (i in row) print row[i]}' > $run_id.p1_p2.s
@@ -79,14 +90,15 @@ comm -12 <(awk '{print $2"\t"$1}' $run_id.p2_p1.s | sort -k1) <(awk '{print $1"\
 > $OUTPUT_NAME
 
 # remove intermediate files :
-echo " - cleaning temporary files" | tee -a LOG.$OUTPUT_NAME
+echo " - cleaning temporary files" | tee -a $OUTPUT_NAME.log
 rm $run_id.*
 
 # write output message :
 amount_mbh=($(wc -l $OUTPUT_NAME))
-echo "---------------------------------------------" | tee -a LOG.$OUTPUT_NAME
-echo "   Done : found $amount_mbh mutual best hits." | tee -a LOG.$OUTPUT_NAME
-echo "   Output written in $OUTPUT_NAME" | tee -a LOG.$OUTPUT_NAME
-echo "   Log written in LOG.$OUTPUT_NAME" | tee -a LOG.$OUTPUT_NAME
-echo "---------------------------------------------" | tee -a LOG.$OUTPUT_NAME
+echo "---------------------------------------------" | tee -a $OUTPUT_NAME.log
+echo "   Done : found $amount_mbh mutual best hits." | tee -a $OUTPUT_NAME.log
+echo "   Output written in $OUTPUT_NAME" | tee -a $OUTPUT_NAME.log
+echo "   Log written in $OUTPUT_NAME.log" | tee -a $OUTPUT_NAME.log
+echo "---------------------------------------------" | tee -a $OUTPUT_NAME.log
+
 ######################### DONE
